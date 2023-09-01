e3f7b7101deb2a264243d6697d800d561ba6a057
ZapeeoSheikh <muhammadrmz871@gmail.com>
2023-09-01 23:37:15 +0500

    operators

---
+++ operators.py
@@ -0,0 +1,13 @@
+add = 23 + 12 
+sub = 23 * 12 
+mul = 23 - 12 
+div = 23 / 12 
+divfloor = 23 // 12 
+rem = 24 % 12 
+
+print(add) # add
+print(sub) # substract
+print(mul) # multiply
+print(div) # divide
+print(divfloor) # floor the division
+print(rem) # remainder
